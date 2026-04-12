d2b52b414717aefeb2ebea3a7ef87960ecacaee4
Yoni <yoniakabecky@gmail.com>
2026-04-12 23:32:30 +0900

    install shadcn-svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import './layout.css';
+	import './styles.css';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,12 @@ const config = {
 		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
 	},
-	kit: { adapter: adapter() }
+	kit: { 
+		adapter: adapter(), 
+		alias: {
+      "@/*": "./path/to/lib/*",
+    },
+	}
 };
 
 export default config;
